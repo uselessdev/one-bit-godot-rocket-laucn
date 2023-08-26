4dba4643c4666a59759b433060f86a523f9e45cb
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2023-08-26 17:46:07 -0300

    rocket launch :rocktet:

---
+++ world.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+# get_node can be also used as $<node name>
+@onready var animation_player = $AnimationPlayer
+@onready var launch_button = $LaunchButton
+@onready var rocket_flare = $Rocket/RocketFlare
+
+func _on_launch_button_button_down():
+	animation_player.play("launch")
+	launch_button.hide()
+	rocket_flare.show()
